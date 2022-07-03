84bcb9af00f88fd9ecb943cd720039917ff84afb
Lingling-Z <97594629+Lingling-Z@users.noreply.github.com>
2022-07-03 22:08:40 +1200

    shopping cart with local data

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
+function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]); // eslint-disable-line react-hooks/exhaustive-deps
+  return (
+    <main>
+      {isOpen && <Modal />}
+
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
